0c65a673c9270354305b6ea2f3191373891d49c7
aadhithbose <aadhi@fasteningcode.com>
2019-12-16 21:23:53 +1300

    first commit

---
+++ 2019Dec/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2019Dec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
