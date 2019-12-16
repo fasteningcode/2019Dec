63c095b59dad18117a0fcc7c0591792cb6f48923
Aadhi <aadhi@fasteningcode.com>
2019-12-16 21:28:20 +1300

    Update Program.cs

--- 2019Dec/Program.cs
+++ 2019Dec/Program.cs
@@ -6,7 +6,22 @@ namespace _2019Dec
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter your first number");
+
+            int a = int.Parse( Console.ReadLine());
+            Console.WriteLine("The number you entered is " + a);
+
+            Console.WriteLine("Enter your second number");
+
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("The number you entered is " + b);
+
+            Console.WriteLine("Sum of 2 numbers are " + add(a,b));
+        }
+
+        private static int add(int x, int y)
+        {
+            return x + y;
         }
     }
 }
