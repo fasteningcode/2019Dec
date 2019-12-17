0a8db1d6c60c4f94225c78aad90f064b31483edd
Aadhi <aadhi@fasteningcode.com>
2019-12-17 21:38:07 +1300

    static method for login

--- 2019Dec/Program.cs
+++ 2019Dec/Program.cs
@@ -15,15 +15,36 @@ namespace _2019Dec
             IWebDriver driver = new ChromeDriver();
             driver.Navigate().GoToUrl("http://horse-dev.azurewebsites.net/Account/Login?ReturnUrl=%2f");
 
-            //identify & enter UN & PW
-            IWebElement userName = driver.FindElement(By.Id("UserName"));
-            userName.SendKeys("hari");
-
-            //click Login
+            //accessing static method
+            LoginPage.LoginSuccess(driver);
 
             // verify that the username is displayed
-        }
+            IWebElement txtUserName =  driver.FindElement(By.XPath("//a[@href='#'][contains(.,'Hello hari!')]"));
 
+            if (txtUserName.Text == "Hello hari!")
+                Console.WriteLine("Verified the username");
+            else
+                Console.WriteLine("Test failed to find the username");
+
+            // click on adminstration
+            driver.FindElement(By.XPath("//a[contains(.,'Administration')]")).Click();
+
+            // click on time n materials
+            driver.FindElement(By.XPath("//a[@href='/TimeMaterial']")).Click();
+
+            // click create new
+
+
+            // enter valid information and click save
+
+            //verify that the time n material object is displayed in the table
+
+            //assignment
+            // complete 3 test cases
+            // create create time and material and verify
+            // edit create time and material and verify
+            // delete create time and material and verify
+        }
 
     }
 }   
